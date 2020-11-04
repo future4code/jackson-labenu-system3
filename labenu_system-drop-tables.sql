18564e1a32bc9093643216759e02778a91730024
RobertoDeAbreuSalgado <r.salgado3@gmail.com>
2020-11-04 20:52:19 -0300

    Adicionando .gitignore e scripts iniciais do db.

---
+++ labenu_system-drop-tables.sql
@@ -0,0 +1,13 @@
+DROP TABLE teacher_specialty_labenu_system;
+
+DROP TABLE specialty_labenu_system;
+
+DROP TABLE student_hobby_labenu_system;
+
+DROP TABLE hobby_labenu_system;
+
+DROP TABLE teacher_labenu_system;
+
+DROP TABLE student_labenu_system;
+
+DROP TABLE mission_labenu_system;
